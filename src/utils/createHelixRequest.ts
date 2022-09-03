144b3c1c4f764f5a985ea3cd8f46d6fc18e1271f
Jesper Lundgren <koudelkaa@gmail.com>
2022-09-04 01:10:59 +0200

    test

--- src/utils/createHelixRequest.ts
+++ src/utils/createHelixRequest.ts
@@ -1,4 +1,5 @@
-export const createHelixRequest = async (request: Request) => {
+import {type Request as HelixRequest} from 'graphql-helix'
+export const createHelixRequest = async (request: Request): Promise<HelixRequest> => {
   const url = new URL(request.url)
   const query = Object.fromEntries(new URLSearchParams(url.search))
 
@@ -6,7 +7,7 @@ export const createHelixRequest = async (request: Request) => {
     request.method === 'POST' ? await request.json() : undefined
 
   return {
-    body,
+    body: body,
     headers: request.headers,
     method: request.method,
     query,
